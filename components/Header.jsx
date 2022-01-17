a848a396aa1586aa0244d9b21446d47b610f4d7d
derickson.loss <derickson.loss@paylivre.com>
2022-01-17 19:07:39 -0300

    fix styling typo

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,14 +14,14 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              GraphBlog/
+              GraphBlog
             </span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md-float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
