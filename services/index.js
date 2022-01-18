728ffb91ba27f4e4f23e0b4359a8ca15674755cf
derickson.loss <dericksonlossit@gmail.com>
2022-01-18 14:18:07 -0300

    implements featured posts fetching function

--- services/index.js
+++ services/index.js
@@ -158,3 +158,28 @@ export const getComments = async (slug) => {
 
   return result.comments;
 };
+
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetCategoryPost() {
+      posts(where: {featuredPost: true}) {
+        author {
+          name
+          photo {
+            url
+          }
+        }
+        featuredImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }   
+  `;
+
+  const result = await request(graphQlAPI, query);
+
+  return result.posts;
+};
